250bb73d4edf51967d5e969dfcb899fe3e9e0043
HarryCoburn <harryjcoburn@gmail.com>
2026-03-22 20:14:51 -0400

    textnode, main script

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+
+def main():
+    new_node = TextNode("Here is a URL", TextType.LINK, "https://www.boot.dev")
+    print(new_node)
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text  # The text content of the node
+        self.text_type = text_type  # the TextType of the node
+        self.url = url  # URL of link or image, or None
+
+    def __eq__(self, other):
+        if other is not TextNode:
+            return False
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
